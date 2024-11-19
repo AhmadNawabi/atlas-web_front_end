0ed9e7677106854944819166cd8eb9e0e7ae9c20
AhmadNawabi <106168136+AhmadNawabi@users.noreply.github.com>
2024-11-19 08:31:24 -0500

    Delete Javascript_advanced directory

--- Javascript_advanced/2-function_me.js
+++
@@ -1,9 +0,0 @@
-function welcomeMessage(fullName) {
-  return function closure(fullName) {
-    alert(`Welcome ${fullName}`);
-  };
-}
-
-const guillaum = welcomeMessage("Guillaume");
-const alex = welcomeMessage("Alex");
-const fred = welcomeMessage("Fred");
